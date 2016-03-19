4c3b0fb4014fecfe78b31be35ad44ad3f4ac39f0
Nikhil. R <rnikhil.2014@gmail.com>
2016-03-19 17:39:24 +0530

    Delete old blog

--- README.md
+++
@@ -1,4 +0,0 @@
-# rnikhil275.github.io
-rnikhil275.github.io
-
-Still under construction. 
--- js/main.js
+++
@@ -1,7 +0,0 @@
-function get_email_link (link_title) {
-	var username = "rnikhil275";
-	var hostname = "gmail.com";
-	var link = username + "@" + hostname;
-	link_title = link_title || link;
-	document.write("<a href='" + "mail" + "to:" + link + "''>"+link_title + "</a>" )
-}
